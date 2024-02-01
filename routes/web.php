f7faa7b12ae13ec73a189ec126bfb56c662ebb56
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-02-01 13:23:15 -0300

    Rotas e views

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
 Route::get('/login', function() {
     return 'login';
@@ -25,16 +15,31 @@ Route::prefix('/app')->group(function () {
 
     Route::get('/clientes', function() {
         return 'clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function() {
         return 'fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function() {
-        return 'fornecedores';
-    });
+        return 'produtos';
+    })->name('app.produtos');
 });
+
+Route::get('/rota1', function() {
+    echo 'rota1';
+} )->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+} )->name('site.rota2');
+
+//Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function () {
+   echo 'A rota acessada não existe. <a href="'.\route('site.index').'" ">Clique aqui<a/> para ir para página inicial';
+});
+
 
 
 
